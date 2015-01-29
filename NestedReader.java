6430f6480ba062156fef7931a42f3b6fc81135cb
Morgan Zhang <Morgan@Morgans-MBP.att.net>
2015-01-29 15:19:18 -0800

    V0.19 Update NestedReader

--- NestedReader.java
+++ NestedReader.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Stack;
 
 /**
  * Created by Morgan on 1/28/15.
@@ -9,9 +10,11 @@ public class NestedReader {
     StringBuilder buf;    // fill this as you process, character by character
     BufferedReader input; // where are we reading from?
     int c; // current character of lookahead; reset upon each getNestedString() call
+    Stack<Integer> s;
 
     public NestedReader() {
         this.buf = new StringBuilder();
+        s = new Stack();
     }
 
     public void getNewInput(BufferedReader input) throws IOException {
@@ -32,12 +35,45 @@ public class NestedReader {
     int consume() throws IOException {
         int flag = 0;
         buf.append((char)c);
-        if(c == 47) flag = 1;
+        if(c == (int)'\\') flag = 1;
+
+        switch (c) {
+            case (int)'(':
+                s.push((int)')');
+                break;
+            case (int)'[':
+                s.push((int)']');
+                break;
+            case (int)'{':
+                s.push((int)'}');
+                break;
+
+            case (int)')':
+                if(s.peek() == (int)')') s.pop();
+                else {
+                    return -1;
+                }
+                break;
+            case (int)']':
+                if(s.peek() == (int)']') s.pop();
+                else {
+                    return -1;
+                }
+                break;
+            case (int)'}':
+                if(s.peek() == (int)'}') s.pop();
+                else {
+                    return -1;
+                }
+                break;
+        }
+
         c = input.read();
-        if(flag == 1 && c == 47) {
+        if(flag == 1 && c == (int)'\\') {
             buf.deleteCharAt(buf.length()-1);
             return -1;
         }
+
         return 0;
     }
 }
